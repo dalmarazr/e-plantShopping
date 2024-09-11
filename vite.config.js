f9b7256ea1ab5a775430f5416996b397571eb438
dalmarazr <tec.almz@gmail.com>
2024-09-11 15:05:32 -0600

    chore: removing comment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/e-plantShopping/",
   base: 'https://dalmarazr.github.io/e-plantShopping/',
 })
